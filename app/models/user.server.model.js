12b4da7ea219da9fc59b0058ae21efa7a917e4ba
samuelpack <packmac2@gmail.com>
2017-12-04 14:09:11 -0500

    custom getter modifiers works

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -5,8 +5,30 @@ const UserSchema = new Schema ({
     firstName: String,
     lastName: String,
     email: String,
-    username: String,
-    password: String
+    username: {
+        type: String,
+        trim: true
+    },
+    password: String,
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    website: {
+        type: String,
+        get: function(url) {
+            if (!url) {
+                return url;
+            } else {
+                if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+                    url = 'http://' + url;
+                }
+                return url;
+            }
+        },
+        default: 'http://www.test_example.com'
+    },
 });
 
+UserSchema.set('toJSON',{getters: true}); 
 mongoose.model('User', UserSchema);
